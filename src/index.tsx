667e992e0dc778e32fcc4d7686202fee5ee83b20
seongg <seongeun7877@naver.com>
2023-07-27 16:49:51 +0900

    [style] global font 추가

--- src/index.tsx
+++ src/index.tsx
@@ -1,13 +1,13 @@
-import React from "react";
-import ReactDOM from "react-dom/client";
-import "./index.css";
-import App from "./App";
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import './index.css';
+import App from './App';
+import GlobalStyle from './styles/global';
 
-const root = ReactDOM.createRoot(
-  document.getElementById("root") as HTMLElement
-);
+const root = ReactDOM.createRoot(document.getElementById('root') as HTMLElement);
 root.render(
-  <React.StrictMode>
+  <div>
+    <GlobalStyle />
     <App />
-  </React.StrictMode>
+  </div>,
 );
