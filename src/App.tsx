69806759992be106b8e32915079059add279eca9
Your Name <90436@naver.com>
2023-07-28 16:10:14 +0900

    [style] 가로- 세로 화면 구성 구분

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,30 @@
 import React from 'react';
 import './App.css';
 import Header from './components/Header';
+import { useMediaQuery } from 'react-responsive';
+
+// 세로 모드
+const TabletPortrait = ({ children }: any) => {
+  const isTablet = useMediaQuery({ minWidth: 768, maxWidth: 999 });
+  return isTablet ? children : null;
+};
+
+// 가로 모드  - 우선 순위
+const TabletLandscape = ({ children }: any) => {
+  const isTablet = useMediaQuery({ minWidth: 1000, maxWidth: 1200 });
+  return isTablet ? children : null;
+};
 
 function App() {
   return (
     <div className="App">
-      <Header />
+      <TabletPortrait>
+        <Header type="portrait" />
+      </TabletPortrait>
+
+      <TabletLandscape>
+        <Header type="landscape" />
+      </TabletLandscape>
     </div>
   );
 }
