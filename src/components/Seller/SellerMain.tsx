6b5b6411f43dcc9e17d6ee36f3edd9e911a5e00a
Your Name <90436@naver.com>
2023-08-01 17:26:09 +0900

    [feature] seller main page 구성

---
+++ src/components/Seller/SellerMain.tsx
@@ -0,0 +1,56 @@
+import React from 'react';
+import SellerHeader from './SellerHeader';
+import { Route, Router, Routes } from 'react-router-dom';
+import LandscapeMain from '../LandscapeMain';
+import SellerProductList from './SellerProductList';
+
+function SellerMain(props: any) {
+  const style = String(props.type);
+  console.log(style);
+
+  function StyleType(style: any) {
+    if (style == 'portrait') {
+      return 'portrait';
+    } else {
+      return 'landscape';
+    }
+  }
+
+  return (
+    <>
+      <SellerHeader type={StyleType(style)} />
+      <Routes>
+        {/* 상품 */}
+        <Route path="/product" element={<SellerProductList type={'landscape'} />}></Route>
+        {/* 발주 */}
+        <Route path="/order" element={<SellerProductList type={'landscape'} />}></Route>
+        <Route path="/order/list" element={<SellerProductList type={'landscape'} />}></Route>
+        {/* 입고 */}
+        <Route path="/import" element={<SellerProductList type={'landscape'} />}></Route>
+        <Route path="/import/pre" element={<SellerProductList type={'landscape'} />}></Route>
+        <Route path="/import/pre/list" element={<SellerProductList type={'landscape'} />}></Route>
+        <Route path="/import/pre/register" element={<SellerProductList type={'landscape'} />}></Route>
+        <Route path="/import/search" element={<SellerProductList type={'landscape'} />}></Route>
+        <Route path="/import/list" element={<SellerProductList type={'landscape'} />}></Route>
+        {/* 출고 */}
+        <Route path="/export" element={<SellerProductList type={'landscape'} />}></Route>
+        <Route path="/export/list" element={<SellerProductList type={'landscape'} />}></Route>
+        {/* 매칭 */}
+        <Route path="/match" element={<SellerProductList type={'landscape'} />}></Route>
+        <Route path="/match/list" element={<SellerProductList type={'landscape'} />}></Route>
+        {/* 마이페이지 */}
+        <Route path="/mypage" element={<SellerProductList type={'landscape'} />}></Route>
+        {/* 마케팅 */}
+        <Route path="/marketing" element={<SellerProductList type={'landscape'} />}></Route>
+        <Route path="/marketing/search" element={<SellerProductList type={'landscape'} />}></Route>
+        <Route path="/marketing/product" element={<SellerProductList type={'landscape'} />}></Route>
+        <Route path="/marketing/danger" element={<SellerProductList type={'landscape'} />}></Route>
+        <Route path="/marketing/channel" element={<SellerProductList type={'landscape'} />}></Route>
+        {/* 쇼핑몰 */}
+        <Route path="/shop" element={<SellerProductList type={'landscape'} />}></Route>
+      </Routes>
+    </>
+  );
+}
+
+export default SellerMain;
