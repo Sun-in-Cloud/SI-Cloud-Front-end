fe15c013825dd7047f441dc1a855fa216a7d280e
seongg <seongeun7877@naver.com>
2023-08-03 10:01:12 +0900

    [feat] 상품관리-상품 목록 조회 기능

--- src/components/3pl/Threepl_ProductList.tsx
+++ src/components/3pl/Threepl_ProductList.tsx
@@ -3,51 +3,105 @@ import Sidebar from './Sidebar';
 import ListingPage from '../ListingPage';
 import { styled } from 'styled-components';
 import { Location, useLocation } from 'react-router-dom';
+import { sellerCompany } from '../../global/CompanyInterface';
+import axios from 'axios';
 
 function Threepl_ProductList(props: any) {
-  const com: string[] = [
-    '성은이네 옷장',
-    '유진 아이스크림',
-    '성은 케이크',
-    '성은이네 옷장',
-    '유진 아이스크림',
-    '성은 케이크',
-    '성은이네 옷장',
-    '유진 아이스크림',
+  const columns: string[] = ['바코드 번호', '상품군', '상품명', '충분재고', '안전재고', '현재재고'];
+
+  const rows: any = [
+    {
+      productNo: '0101010',
+      productGroup: '바지',
+      productName: '귀여운 핑크바지',
+      safetyStock: 10,
+      currentStock: 15,
+      enoughStock: 12,
+    },
+    {
+      productNo: '111111',
+      productGroup: '바지',
+      productName: '귀여운 파란바지',
+      safetyStock: 10,
+      currentStock: 15,
+      enoughStock: 12,
+    },
+    {
+      productNo: '22222222',
+      productGroup: '바지',
+      productName: '귀여운 파란바지',
+      safetyStock: 10,
+      currentStock: 15,
+      enoughStock: 12,
+    },
+    {
+      productNo: '333333',
+      productGroup: '바지',
+      productName: '귀여운 파란바지',
+      safetyStock: 10,
+      currentStock: 15,
+      enoughStock: 12,
+    },
+    {
+      productNo: '444444',
+      productGroup: '바지',
+      productName: '귀여운 파란바지',
+      safetyStock: 10,
+      currentStock: 15,
+      enoughStock: 12,
+    },
+    {
+      productNo: '555555',
+      productGroup: '바지',
+      productName: '귀여운 파란바지',
+      safetyStock: 10,
+      currentStock: 15,
+      enoughStock: 12,
+    },
+    {
+      productNo: '6666666',
+      productGroup: '바지',
+      productName: '귀여운 파란바지',
+      safetyStock: 10,
+      currentStock: 15,
+      enoughStock: 12,
+    },
+    {
+      productNo: '777778',
+      productGroup: '바지',
+      productName: '귀여운 파란바지',
+      safetyStock: 10,
+      currentStock: 15,
+      enoughStock: 12,
+    },
+    {
+      productNo: '8888888',
+      productGroup: '바지',
+      productName: '귀여운 파란바지',
+      safetyStock: 10,
+      currentStock: 15,
+      enoughStock: 12,
+    },
+    {
+      productNo: '999999',
+      productGroup: '바지',
+      productName: '귀여운 파란바지',
+      safetyStock: 10,
+      currentStock: 15,
+      enoughStock: 12,
+    },
   ];
 
-  const location: Location = useLocation();
-
-  const [seller, setSeller] = useState('');
-
-  function findSeller(new_seller: any): void {
-    setSeller(new_seller.item);
-    console.log(seller);
-  }
-
-  function submenu(location: Location) {
-    if (location.pathname.includes('/3pl/product')) {
-      return <Sidebar company={com} findSeller={findSeller} />;
-    }
-  }
-
-  useEffect(() => {}, [seller]);
-
   return (
-    <MainPage>
-      <h1>상품목록조회</h1>
-      {submenu(location)}
-      <ListingPage sellerName={seller} />
-      <h1></h1>
-    </MainPage>
+    <ListingPage
+      sellerNo={props.seller}
+      titles={columns}
+      number={[0, 1, 2, 3]}
+      rows={rows}
+      columns={columns.length}
+      onDetail={false}
+    />
   );
 }
-
-const MainPage = styled.div`
-  margin-top: -40px;
-  display: grid;
-  grid-template-columns: 0.5fr 1.6fr 5fr 0.5fr;
-  grid-template-areas: '. . ListingPage .';
-`;
 
 export default Threepl_ProductList;
