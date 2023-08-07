ee7b43695703a5848006e58947c08f3edd7b1d96
seongg <seongeun7877@naver.com>
2023-08-07 12:11:49 +0900

    [feat] 매칭서비스 component 작업

--- src/components/common/matching/Filter.tsx
+++ src/components/common/matching/Filter.tsx
@@ -4,136 +4,41 @@ function SelectTable(props: any) {
   const [selected, setSelected] = useState<string>(props.title);
   const [onList, setOnList] = useState<boolean>(false);
 
-  //const toggleList = () => {
-  //  if (onList === true) setOnList(false);
-  //  else setOnList(true);
-  //};
-
   const clickItem = (item: string) => {
     setSelected(item);
-    //setOnList(false);
-    console.log(item);
+    props.findFilter([props.title[1], item]);
   };
   return (
     <>
-      <Title>{props.title}</Title>
+      <Title>{props.title[0]}</Title>
       <Chips>
         {props.option.map((item: string, idx: number) => {
           return (
             <>
               <Chip
                 onClick={() => {
-                  clickItem(item);
+                  typeof item === 'string' ? clickItem(item) : clickItem(item[1]);
                 }}
                 style={{
-                  backgroundColor: selected === item ? '#1e1008' : '#ffffff',
-                  color: selected === item ? '#ffffff' : '#1e1008',
+                  backgroundColor: selected === (typeof item === 'string' ? item : item[1]) ? '#1e1008' : '#ffffff',
+                  color: selected === (typeof item === 'string' ? item : item[1]) ? '#ffffff' : '#1e1008',
                 }}
               >
-                {item}
+                {typeof item === 'string' ? item : item[0]}
               </Chip>
             </>
           );
-
-          //드롭다운
-          //   return (
-          //     <>
-          //       <Title>{props.title}</Title>
-          //       <TitleBtn onClick={toggleList}>{selected}</TitleBtn>
-
-          //       <ListBox>
-          //         {props.option.map((item: string, idx: number) => {
-          //           return (
-          //             <>
-          //               <List
-          //                 onClick={() => {
-          //                   clickItem(item);
-          //                 }}
-          //                 toggle={onList}
-          //               >
-          //                 {item}
-          //               </List>
-          //             </>
-          //           );
-          //         })}
-          //       </ListBox>
-          //     </>
-          //   );
         })}
       </Chips>
     </>
   );
 }
 
-// interface StyledButtonProps {
-//   readonly toggle: true | false;
-// }
-
 const Title = styled.div`
   font-family: Jalnan;
   font-size: 20px;
   margin-bottom: 10px;
 `;
-// const TitleBtn = styled.button`
-//   width: 200px;
-//   border: 1px solid #c4c4c4;
-//   box-sizing: border-box;
-//   border-radius: 10px;
-//   padding: 12px 13px;
-//   font-family: Dotum_Bold;
-//   font-style: normal;
-//   font-weight: 400;
-//   font-size: 14px;
-//   line-height: 16px;
-//   background: #f4f0df;
-//   // background: url(./img/Polygon_up.png) 93% no-repeat; /*화살표 이미지 삽입*/
-//   appearance: none;
-//   text-align: left;
-
-//   &:focus {
-//     border: 1px solid #9b51e0;
-//     box-sizing: border-box;
-//     border-radius: 10px;
-//     outline: 3px solid #0073ff;
-//     border-radius: 10px;
-//   }
-// `;
-
-// const ListBox = styled.ul`
-//   width: 200px;
-//   list-style: none;
-//   background: #ffffff;
-//   border: 1px solid #c4c4c4;
-//   box-sizing: border-box;
-//   box-shadow: 4px 4px 14px rgba(0, 0, 0, 0.15);
-//   border-radius: 10px;
-//   margin-top: 9px;
-// `;
-
-// const List = styled.li<StyledButtonProps>`
-//   display: ${(props) => {
-//     if (props.toggle === true) return 'block';
-//     else return 'none';
-//   }};
-//   border: none;
-//   background-color: #ffffff;
-//   font-family: Dotum_Medium;
-//   font-style: normal;
-//   font-weight: 400;
-//   font-size: 14px;
-//   line-height: 16px;
-//   padding: 7px 10px;
-//   margin: 5px 7px;
-//   box-sizing: border-box;
-//   text-align: left;
-
-//   &:focus {
-//     background: #f8e4ff;
-//     width: 90%;
-//     border-radius: 8px;
-//     box-sizing: border-box;
-//   }
-// `;
 
 const Chips = styled.div`
   margin-bottom: 10px;
--- src/components/common/matching/SelectTable.tsx
+++ src/components/common/matching/SelectTable.tsx
@@ -1,40 +1,112 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import { styled } from 'styled-components';
 import Filter from './Filter';
 import LoginBtn from '../Loginbtn';
+import { Location, useLocation } from 'react-router-dom';
+import axios from 'axios';
+
 function SelectTable(props: any) {
-  const options1: string[] = ['냉장', '냉동', '의류', '가전제품', '식물'];
+  const location: Location = useLocation();
+
+  const [options1, setOptions1] = useState<string[]>([]);
+
+  async function getGroupList() {
+    const listurl = '/productGroup/list';
+    await axios
+      .get(listurl, {})
+      .then(function (response) {
+        console.log('-', response.data);
+        setOptions1(response.data);
+      })
+      .catch(function (error) {
+        console.log(error);
+      });
+  }
+
   const options2: string[] = [
     '서울',
+    '경기',
     '인천',
-    '대전',
+    '강원',
     '부산',
+    '대전',
     '대구',
     '광주',
+    '충청북도',
+    '충청남도',
+    '전라북도',
+    '전라남도',
+    '경상북도',
+    '경상남도',
     '제주',
-    '경기',
-    '강원',
-    '충북',
-    '충남',
-    '전북',
-    '전남',
-    '경북',
-    '경남',
   ];
-  const options3: string[] = ['1000건 이하', '1000~10000건', '10000건~500000건', '500000건 이상'];
+
+  const options3: string[][] = [
+    ['전체', '0'],
+    ['1000 이상', '1000'],
+    ['10000 이상', '10000'],
+    ['50000 이상', '50000'],
+  ];
+  const options4: string[][] = [
+    ['전체', '0'],
+    ['1000 이상', '1000'],
+    ['10000 이상', '10000'],
+    ['50000 이상', '50000'],
+  ];
+  const options5: string[][] = [
+    ['전체', '0'],
+    ['1달', '1'],
+    ['2달', '2'],
+    ['3달', '3'],
+  ];
+
+  interface filter {
+    productGroup: string;
+    location: string;
+    exportAmount?: number;
+    price?: number;
+    period: number;
+  }
+
+  const initFilter: filter = {
+    productGroup: '',
+    location: '',
+    exportAmount: 0,
+    price: 0,
+    period: 0,
+  };
+
+  const [filters, setFilters] = useState<filter>(initFilter);
+
+  function findFilter(item: string[]): void {
+    const name: string = item[0];
+    const value: string = item[1];
+    setFilters({ ...filters, [name]: value });
+  }
+
+  const getMacthList = () => {
+    console.log(filters);
+  };
+
+  useEffect(() => {
+    getGroupList();
+  }, []);
+
   return (
     <>
       <Table>
         <h1></h1>
         <hr></hr>
-        <Filter title="상품군" option={options1}></Filter>
-        <Filter title="위치" option={options2}></Filter>
-        <Filter title="최소출고건수" option={options3}></Filter>
-        <ChkFilter>
-          <Title>계약 종료된 곳만 보기</Title>
-          <ChkBox type="checkbox"></ChkBox>
-        </ChkFilter>
-        <LoginBtn variant="primary" type="landscape">
+        <Filter title={['상품군', 'productGroup']} option={options1} findFilter={findFilter}></Filter>
+        <Filter title={['위치', 'location']} option={options2} findFilter={findFilter}></Filter>
+        {location.pathname.includes('3pl') && (
+          <Filter title={['출고건수', 'exportAmount']} option={options3} findFilter={findFilter}></Filter>
+        )}
+        {location.pathname.includes('seller') && (
+          <Filter title={['가격', 'price']} option={options4} findFilter={findFilter}></Filter>
+        )}
+        <Filter title={['남은 계약 기간', 'period']} option={options5} findFilter={findFilter}></Filter>
+        <LoginBtn variant="primary" type="landscape" onClick={getMacthList} style={{ marginTop: '20px' }}>
           조회하기
         </LoginBtn>
       </Table>
@@ -49,6 +121,7 @@ const Table = styled.div`
   border-radius: 25px;
   text-align: left;
   padding: 30px;
+  background-color: #f4f0df;
 `;
 
 const Title = styled.div`
@@ -56,30 +129,6 @@ const Title = styled.div`
   font-family: Jalnan;
   font-size: 20px;
   margin-top: 40px;
-`;
-
-const ChkFilter = styled.div`
-  display: flex;
-  align-items: flex-end;
-  margin-bottom: 50px;
-`;
-
-const ChkBox = styled.input`
-  appearance: none;
-  border: 1.5px solid gainsboro;
-  border-radius: 0.35rem;
-  width: 1.5rem;
-  height: 1.5rem;
-  margin-left: 10px;
-
-  &:checked {
-    border-color: transparent;
-    background-image: url("data:image/svg+xml,%3csvg viewBox='0 0 16 16' fill='white' xmlns='http://www.w3.org/2000/svg'%3e%3cpath d='M5.707 7.293a1 1 0 0 0-1.414 1.414l2 2a1 1 0 0 0 1.414 0l4-4a1 1 0 0 0-1.414-1.414L7 8.586 5.707 7.293z'/%3e%3c/svg%3e");
-    background-size: 100% 100%;
-    background-position: 50%;
-    background-repeat: no-repeat;
-    background-color: #0073ff;
-  }
 `;
 
 export default SelectTable;
