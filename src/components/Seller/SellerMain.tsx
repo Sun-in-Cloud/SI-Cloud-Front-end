207edb8b0bb372358fe3176e0838412060ee0056
Your Name <90436@naver.com>
2023-08-07 13:52:42 +0900

    [feat] 라우터 별 파일 설정

--- src/components/Seller/SellerMain.tsx
+++ src/components/Seller/SellerMain.tsx
@@ -5,10 +5,12 @@ import SellerProductList from './product/SellerProductList';
 import SellerProductDetail from './product/SellerProductDetail';
 import SellerProductRegister from './product/SellerProductRegister';
 import SellerOrderList from './order/SellerOrderList';
-import SellerImportList from './import/SellerImportPre';
+import SellerImportList from './import/SellerImportList';
 import SellerExportList from './export/SellerExportList';
 import SellerImportPre from './import/SellerImportPre';
 import SellerImportFixedList from './import/SellerImportFixedList';
+import DetailExport from './export/SellerDetailExport';
+import SellerDetailExport from './export/SellerDetailExport';
 
 function SellerMain(props: any) {
   const style = String(props.type);
@@ -33,11 +35,15 @@ function SellerMain(props: any) {
         {/* 발주 */}
         <Route path="/order/list" element={<SellerOrderList type={'landscape'} />}></Route>
         {/* 입고 */}
+        {/* 입고 예정리스트 조회 - 화주사 확정 */}
         <Route path="/import/pre/list" element={<SellerImportFixedList type={'landscape'} />}></Route>
+        {/* 입고 등록 */}
         <Route path="/import/pre/*" element={<SellerImportPre type={'landscape'} />}></Route>
+        {/* 입고 확정 - 3PL확정 */}
         <Route path="/import/list" element={<SellerImportList type={'landscape'} />}></Route>
         {/* 출고 */}
-        <Route path="/export/*" element={<SellerExportList type={'landscape'} />}></Route>
+        <Route path="/export/list" element={<SellerExportList type={'landscape'} />}></Route>
+        <Route path="/export/*" element={<SellerDetailExport type={'landscape'} />}></Route>
         {/* 매칭 */}
         <Route path="/match" element={<SellerProductList type={'landscape'} />}></Route>
         <Route path="/match/list" element={<SellerProductList type={'landscape'} />}></Route>
