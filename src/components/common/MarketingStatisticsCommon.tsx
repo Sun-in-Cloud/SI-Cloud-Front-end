1c65795d2ba99730ab45fa2073229489161dd96e
Your Name <90436@naver.com>
2023-08-09 19:05:11 +0900

    [feat] 그래프 추가

--- src/components/common/MarketingStatisticsCommon.tsx
+++ src/components/common/MarketingStatisticsCommon.tsx
@@ -1,8 +1,48 @@
+import ApexCharts from 'apexcharts';
 import React from 'react';
 import { styled } from 'styled-components';
+import ApexChart from 'apexcharts';
+import ReactApexChart from 'react-apexcharts';
 
 function MarketingStatisticsCommon(props: any) {
-  return <Graph></Graph>;
+  return (
+    <Graph>
+      <ReactApexChart
+        type="bar"
+        series={[
+          { name: 'Price', data: [1000, 2000, 3000] },
+          { name: 'Price2', data: [1500, 1000, 2500] },
+        ]}
+        options={{
+          theme: { mode: 'light' },
+          chart: {
+            height: 300,
+            width: 500,
+            toolbar: { show: false },
+            background: 'transparent',
+          },
+          stroke: { curve: 'smooth', width: 4 },
+          grid: { show: false },
+          yaxis: { show: false },
+          xaxis: {
+            labels: { show: false },
+            axisTicks: { show: false },
+            axisBorder: { show: false },
+            categories: [1660004640, 1660091040, 1660177440],
+            type: 'datetime',
+          },
+          fill: {
+            type: 'gradient',
+            gradient: { gradientToColors: ['#D8E3F0'], stops: [0, 100] },
+          },
+          colors: ['#BED1E6'],
+          tooltip: {
+            y: { formatter: (value) => `$ ${value.toFixed(2)}` },
+          },
+        }}
+      />
+    </Graph>
+  );
 }
 const Graph = styled.div`
   height: 100%;
