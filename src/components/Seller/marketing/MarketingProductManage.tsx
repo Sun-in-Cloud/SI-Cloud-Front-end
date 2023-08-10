edee28314e906e9ce9ed132f3dcf7206ae263be7
YYJ-1229 <90436@naver.com>
2023-08-11 01:14:35 +0900

    [feat] 마케팅 상품관리 기능 완료

--- src/components/Seller/marketing/MarketingProductManage.tsx
+++ src/components/Seller/marketing/MarketingProductManage.tsx
@@ -1,7 +1,247 @@
-import React from 'react';
+import React, { useCallback, useEffect, useState } from 'react';
+import styled from 'styled-components';
+import axios from 'axios';
+
+import Tab from '../../common/Tab';
+import MarketingChartWeekly from './MarketingChartWeekly';
+import MarketingChartMonthly from './MarketingChartMonthly';
+import MarketingChartYearly from './MarketingChartYearly';
+import LoginBtn from '../../common/Loginbtn';
+import Modal from '../../common/Modal';
+import SellerMarketingSerch from './SellerMarketingSerch';
 
 function MarketingProductManage(props: any) {
-  return <div></div>;
+  const [product, setProduct] = useState<Array<string>>([]);
+  const [isSearchModal, setSearchModal] = useState<boolean>(false);
+  const [weeklyType, setWeeklyType] = useState('weeklyCount');
+  const [monthlyType, setMonthlyType] = useState('monthlyCount');
+  const [yearlyType, setyearlyType] = useState('yearlyCount');
+
+  const [data, setData] = useState<any>();
+
+  async function getMarketingByPro() {
+    const listurl = '/seller/marketing/statistics';
+    await axios
+      .get(listurl, {
+        params: {
+          sellerNo: 2,
+        },
+      })
+      .then(function (response) {
+        console.log(response.data);
+        setData(response.data);
+      })
+      .catch(function (error) {
+        console.log(error);
+      });
+  }
+
+  function onChangeTab(e: any) {
+    const name = e.target.name;
+    if (name.includes('week')) {
+      setWeeklyType(name);
+    } else if (name.includes('month')) {
+      setMonthlyType(name);
+    } else {
+      setyearlyType(name);
+    }
+  }
+
+  function getProductNo(props: string[]) {
+    setProduct(props);
+  }
+
+  function onclick(): void {
+    setSearchModal(true);
+  }
+
+  const onClickSearchModal = useCallback(() => {
+    setSearchModal(!isSearchModal);
+    setProduct([]);
+  }, [isSearchModal]);
+
+  useEffect(() => {
+    getMarketingByPro();
+  }, [product]);
+
+  return (
+    <StatisticsPage>
+      <Search>
+        <SearchForm value={product[1]} onFocus={onclick}></SearchForm>
+        <LoginBtn variant="dark" type="landscape" onClick={() => onclick()}>
+          상품찾기
+        </LoginBtn>
+      </Search>
+      {isSearchModal && (
+        <Modal onClickToggleModal={onClickSearchModal}>
+          <SellerMarketingSerch getProductNo={getProductNo} onClickToggleModal={onClickSearchModal} />
+        </Modal>
+      )}
+      {data && (
+        <>
+          <Weekly>
+            <StaticsWeekly>
+              <Btns>
+                <Tab
+                  variant="primary"
+                  bg={`${weeklyType === 'weeklyCount' ? 'primary' : 'basic'}`}
+                  name="weeklyCount"
+                  onClick={(e) => onChangeTab(e)}
+                >
+                  판매수
+                </Tab>
+                <Tab
+                  variant="secondary"
+                  bg={`${weeklyType === 'weeklySales' ? 'secondary' : 'basic'}`}
+                  name="weeklySales"
+                  onClick={(e) => onChangeTab(e)}
+                >
+                  매출
+                </Tab>
+                <WeekItem>Weekly</WeekItem>
+              </Btns>
+              <MarketingChartWeekly type={weeklyType} data={data} />
+            </StaticsWeekly>
+          </Weekly>
+
+          <Monthly>
+            <Statics>
+              <Btns>
+                <Tab
+                  variant="primary"
+                  bg={`${monthlyType === 'monthlyCount' ? 'primary' : 'basic'}`}
+                  name="monthlyCount"
+                  onClick={(e) => onChangeTab(e)}
+                >
+                  판매수
+                </Tab>
+                <Tab
+                  variant="secondary"
+                  bg={`${monthlyType === 'monthlySales' ? 'secondary' : 'basic'}`}
+                  name="monthlySales"
+                  onClick={(e) => onChangeTab(e)}
+                >
+                  매출
+                </Tab>
+                <Item>Monthly</Item>
+              </Btns>
+              <MarketingChartMonthly type={monthlyType} data={data} />
+            </Statics>
+          </Monthly>
+
+          <Yearly>
+            <Statics>
+              <Btns>
+                <Tab
+                  variant="primary"
+                  bg={`${yearlyType === 'yearlyCount' ? 'primary' : 'basic'}`}
+                  name="yearlyCount"
+                  onClick={(e) => onChangeTab(e)}
+                >
+                  판매수
+                </Tab>
+                <Tab
+                  variant="secondary"
+                  bg={`${yearlyType === 'yearlySales' ? 'secondary' : 'basic'}`}
+                  name="yearlySales"
+                  onClick={(e) => onChangeTab(e)}
+                >
+                  매출
+                </Tab>
+                <Item>Yearly</Item>
+              </Btns>
+              <MarketingChartYearly type={yearlyType} data={data} />
+            </Statics>
+          </Yearly>
+        </>
+      )}
+    </StatisticsPage>
+  );
 }
+const StatisticsPage = styled.div`
+  margin-top: -45px;
+  height: 480px;
+  display: grid;
+  grid-template-columns: 0.5fr 3.5fr 2fr 0.5fr;
+  grid-template-rows: 45px 2fr 2fr;
+  grid-template-areas:
+    '. search . .'
+    '. weekly monthly .'
+    '. weekly yearly .';
+`;
+const Search = styled.div`
+  display: flex;
+  margin-top: -15px;
+  padding: 10px;
+  grid-area: search;
+  z-index: 3;
+`;
+
+const SearchForm = styled.input`
+  margin-left: 10px;
+  background: #fff;
+  width: 250px;
+  height: 80%;
+  border: 2px solid black;
+  border-radius: 10px;
+  font-size: 13px;
+  font-family: 'Jalnan';
+  margin: 0 10px;
+`;
+
+const Weekly = styled.div`
+  grid-area: weekly;
+  display: grid;
+  padding: 5px;
+`;
+const Monthly = styled.div`
+  grid-area: monthly;
+  display: grid;
+  padding: 5px 10px;
+`;
+const Yearly = styled.div`
+  grid-area: yearly;
+  display: grid;
+  padding: 5px 10px;
+`;
+
+const StaticsWeekly = styled.div`
+  display: grid;
+  grid-template-rows: 40px 400px;
+`;
+
+const Statics = styled.div`
+  display: grid;
+  grid-template-rows: 40px 250px;
+`;
+
+const Btns = styled.div`
+  height: 40px;
+  display: flex;
+  background-color: tansparent;
+  margin-left: 10px;
+  align-items: center;
+`;
+
+const Item = styled.div`
+  font-size: 17px;
+  font-family: 'Jalnan';
+  letter-spacing: 2px;
+  margin-left: 30px;
+  display: flex;
+  justify-content: flex-start;
+  width: 120px;
+`;
+
+const WeekItem = styled.div`
+  font-size: 17px;
+  font-family: 'Jalnan';
+  letter-spacing: 2px;
+  margin-left: 150px;
+  display: flex;
+  justify-content: flex-end;
+  padding-right: 20px;
+  width: 200px;
+`;
 
 export default MarketingProductManage;
---
+++ src/components/Seller/marketing/SellerMarketingSerch.tsx
@@ -0,0 +1,121 @@
+import React, { useEffect, useRef, useState } from 'react';
+import TableRow from '../../TableRow';
+import RegisterNew from '../../common/RegisterNew';
+import { styled } from 'styled-components';
+import LoginBtn from '../../common/Loginbtn';
+import TableColumn from '../../TableColumn';
+import axios from 'axios';
+
+function SellerMarketingSerch(props: any) {
+  const titles = [
+    ['상품명', 'productName'],
+    ['상품번호', 'productNo'],
+  ];
+  const searchProduct = useRef<HTMLInputElement>(null);
+
+  const [row, setRow] = useState<any>();
+  const [pro, setPro] = useState<string>();
+
+  function getNum(e: string[]) {
+    props.getProductNo(e);
+  }
+
+  function initRow() {
+    setRow([]);
+  }
+
+  function onSubmit(e: React.FormEvent) {
+    e.preventDefault();
+    const pro = searchProduct.current!.value;
+    setPro(pro);
+  }
+
+  async function getProduct() {
+    const listurl = '/seller/import/search';
+    const sellerNo = 2;
+    await axios
+      .get(listurl, {
+        params: {
+          sellerNo: sellerNo,
+          productName: pro,
+        },
+      })
+      .then(function (response) {
+        setRow(response.data);
+      })
+      .catch(function (error) {
+        console.log(error);
+      });
+  }
+
+  useEffect(() => {
+    getProduct();
+  }, [pro]);
+
+  return (
+    <>
+      <ImportModal>
+        <ModalDiv>
+          <SearchDiv onSubmit={onSubmit}>
+            <RegisterNew type="text" ref={searchProduct} />
+            <LoginBtn variant="primary" type="landscape">
+              검색
+            </LoginBtn>
+          </SearchDiv>
+
+          <TableColumn title={titles} columns={titles.length} />
+          {row && (
+            <RowDiv>
+              <TableRow
+                title={titles}
+                rows={row}
+                columns={titles.length}
+                onDetail={true}
+                getProductNo={props.getProductNo}
+                onClickToggleModal={props.onClickToggleModal}
+                initRow={initRow}
+                getNum={getNum}
+              />
+            </RowDiv>
+          )}
+        </ModalDiv>
+      </ImportModal>
+    </>
+  );
+}
+const ImportModal = styled.div`
+  display: grid;
+  width: 100%;
+  height: 100%;
+  align-items: start;
+  justify-items: center;
+`;
+
+const ModalDiv = styled.div`
+  display: grid;
+  width: 85%;
+`;
+
+const RowDiv = styled.div`
+  display: grid;
+  margin-left: 10px;
+  overflow-x: hidden;
+  overflow-y: hidden;
+  height: 300px;
+`;
+
+const SearchDiv = styled.form`
+  display: grid;
+  width: 450px;
+  grid-template-columns: 4fr 0.8fr;
+  padding: 10px;
+  margin: 5px;
+`;
+
+const Btns = styled.div`
+  display: flex;
+  justify-content: flex-end;
+  margin-right: 10px;
+`;
+
+export default SellerMarketingSerch;
